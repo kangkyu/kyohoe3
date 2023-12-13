88f68e6035d515f21c4b3ab7963abcd03284c45e
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-12-13 00:24:02 -0800

    Styling

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,5 +1,3 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,34 +12,43 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.datetime.Clock
 import kotlinx.datetime.IllegalTimeZoneException
-import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
         var location by remember { mutableStateOf("Europe/Paris") }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
-        Column {
-            Text(timeAtLocation)
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Text(
+                timeAtLocation,
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
             TextField(
                 value = location,
-                onValueChange = { location = it },
+                modifier = Modifier.padding(top = 10.dp),
+                onValueChange = { location = it }
             )
-            Button(onClick = {
-                timeAtLocation = currentTimeAt(location) ?: "Invalid location"
-            }) {
+            Button(
+                modifier = Modifier.padding(top = 10.dp),
+                onClick = {
+                    timeAtLocation = currentTimeAt(location) ?: "Invalid location"
+                }
+            ) {
                 Text("Show Time At Location")
             }
         }
